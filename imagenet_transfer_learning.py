507977c53fa656ea9d809061b5d9499c5072d28a
LIM HYUN SEOK <cr3ux53c@gmail.com>
2021-10-27 12:05:23 +0000

    refactoring

--- imagenet_transfer_learning.py
+++ imagenet_transfer_learning.py
@@ -41,7 +41,7 @@ import time
 import os
 import copy
 
-plt.ion()   # interactive mode
+# plt.ion()   # interactive mode
 
 ######################################################################
 # Load Data
@@ -85,8 +85,8 @@ data_dir = 'datasets/hymenoptera_data'
 image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
                                           data_transforms[x])
                   for x in ['train', 'val']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=1,
-                                             shuffle=True, num_workers=16)
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                             shuffle=True, num_workers=4)
               for x in ['train', 'val']}
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
 class_names = image_datasets['train'].classes
@@ -113,12 +113,12 @@ def imshow(inp, title=None):
 
 
 # Get a batch of training data
-inputs, classes = next(iter(dataloaders['train']))
+# inputs, classes = next(iter(dataloaders['train']))
 
 # Make a grid from batch
-out = torchvision.utils.make_grid(inputs)
+# out = torchvision.utils.make_grid(inputs)
 
-imshow(out, title=[class_names[x] for x in classes])
+# imshow(out, title=[class_names[x] for x in classes])
 
 
 ######################################################################
@@ -215,7 +215,7 @@ def visualize_model(model, num_images=6):
     was_training = model.training
     model.eval()
     images_so_far = 0
-    fig = plt.figure()
+    # fig = plt.figure()
 
     with torch.no_grad():
         for i, (inputs, labels) in enumerate(dataloaders['val']):
@@ -227,10 +227,10 @@ def visualize_model(model, num_images=6):
 
             for j in range(inputs.size()[0]):
                 images_so_far += 1
-                ax = plt.subplot(num_images//2, 2, images_so_far)
-                ax.axis('off')
-                ax.set_title('predicted: {}'.format(class_names[preds[j]]))
-                imshow(inputs.cpu().data[j])
+                # ax = plt.subplot(num_images//2, 2, images_so_far)
+                # ax.axis('off')
+                # ax.set_title('predicted: {}'.format(class_names[preds[j]]))
+                # imshow(inputs.cpu().data[j])
 
                 if images_so_far == num_images:
                     model.train(mode=was_training)
@@ -326,8 +326,8 @@ model_conv = train_model(model_conv, criterion, optimizer_conv,
 
 visualize_model(model_conv)
 
-plt.ioff()
-plt.show()
+# plt.ioff()
+# plt.show()
 
 ######################################################################
 # Further Learning
